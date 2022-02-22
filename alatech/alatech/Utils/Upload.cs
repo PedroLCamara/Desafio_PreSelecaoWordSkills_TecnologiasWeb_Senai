04adc8876814a1b423010e6bd4a208509c1032f8
PedroLCamara <plcrlschool@gmail.com>
2022-02-21 21:33:15 -0300

    CriacaoUtilUpload

--- alatech/alatech/Utils/Upload.cs
+++ alatech/alatech/Utils/Upload.cs
@@ -1,10 +1,87 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using System;
 using System.IO;
+using System.Linq;
+using System.Net.Http.Headers;
 
 namespace alatech.Utils
 {
     public static class Upload
     {
+        public static string UploadFile(IFormFile arquivo, int IdObjImg)
+        {
+            try
+            {
+                var pasta = Path.Combine("StaticFiles", "Images");
+                var caminho = Path.Combine(Directory.GetCurrentDirectory(), pasta);
+
+                if (arquivo.Length > 0)
+                {
+                    var fileName = ContentDispositionHeaderValue.Parse(arquivo.ContentDisposition).FileName.Trim('"');
+
+                    if (ValidarExtensao(fileName))
+                    {
+                        int IdImg = 1;
+                        bool UniqueId = false;
+                        var Diretorio = Directory.EnumerateDirectories(caminho).ToList();
+                        while (UniqueId == false)
+                        {
+                            IdImg++;
+
+                            if (Diretorio.Find(a => a.Split(".")[0] == UniqueId.ToString()) == null)
+                            {
+                                UniqueId = true;
+                            }
+                        }
+                        var extensao = RetornarExtensao(fileName);
+                        var novoNome = $"{IdImg}.{extensao}";
+                        var caminhoCompleto = Path.Combine(caminho, novoNome);
+
+                        using (var stream = new FileStream(caminhoCompleto, FileMode.Create))
+                        {
+                            arquivo.CopyTo(stream);
+                        }
+
+                        return novoNome;
+                    }
+
+                    return "Extensão não permitida";
+                }
+                return "";
+            }
+            catch (Exception ex)
+            {
+                return ex.ToString();
+            }
+        }
+
+        public static bool ValidarExtensao(string nomeDoArquivo)
+        {
+            string[] dados = nomeDoArquivo.Split(".");
+            string extensao = dados[dados.Length - 1];
+
+                if (extensao == "png")
+                {
+                    return true;
+                }
+            return false;
+        }
+
+        public static string RetornarExtensao(string nomeDoArquivo)
+        {
+            string[] dados = nomeDoArquivo.Split(".");
+            return dados[dados.Length - 1];
+        }
+
+        public static void RemoverArquivo(string nomeDoArquivo)
+        {
+            var pasta = Path.Combine("StaticFiles", "Images");
+            var caminho = Path.Combine(Directory.GetCurrentDirectory(), pasta);
+            var caminhoCompleto = Path.Combine(caminho, nomeDoArquivo);
+
+            File.Delete(caminhoCompleto);
+        }
+
         public static string BuscarEConverterImagem(int IdImg)
         {
             string NomeArquivo = IdImg.ToString() + ".png";
